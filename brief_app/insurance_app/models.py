7efe3abe9fb96c1fe18293ee52aedc257de711f6
MichAdebayo <adebayomichael4@gmail.com>
2025-01-21 15:52:40 +0100

    add gitignore

--- brief_app/insurance_app/models.py
+++ brief_app/insurance_app/models.py
@@ -1,5 +1,9 @@
 from django.db import models
 
+class Login_and_SignUp(models.Model):
+    user_name = models.CharField(max_length=100)
+    password = models.CharField(max_length=100)
+
 class UserProfile(models.Model):
     first_name = models.CharField(max_length=100)
     surname_name = models.CharField(max_length=100)
@@ -10,17 +14,12 @@ class UserProfile(models.Model):
     weight = models.CharField(max_length=200)
     height = models.CharField(max_length=200) 
     sex = models.CharField(max_length=200)
-
+    user_name = models.ForeignKey(Login_and_SignUp, null=True, on_delete=models.SET_NULL)
+    
     def __str__(self):
         return self.user_name
     
-class Login_and_SignUp(models.Model):
-    user_name = models.CharField(max_length=100)
-    password = models.CharField(max_length=100)
 
 class SignUp(models.Model):
     user_name = models.CharField(max_length=100)
     password = models.CharField(max_length=100)
-
-    
-
