6f1f6bf45301dc2c3dc9c48743fd8e0b0850e757
MichAdebayo <adebayomichael4@gmail.com>
2025-01-21 16:09:59 +0100

    define models

--- brief_app/insurance_app/models.py
+++ brief_app/insurance_app/models.py
@@ -1,24 +1,33 @@
 from django.db import models
 
-class Login_and_SignUp(models.Model):
+class SignUp(models.Model):
     user_name = models.CharField(max_length=100)
+    email = models.CharField(max_length=100)
     password = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.email
+    
+class Login(models.Model):
+    email = models.ForeignKey(SignUp, null=True, on_delete=models.SET_NULL)
+    password = models.ForeignKey(SignUp, null=True, on_delete=models.SET_NULL)
+    
+    def __str__(self):
+        return self.email
+
 class UserProfile(models.Model):
-    first_name = models.CharField(max_length=100)
-    surname_name = models.CharField(max_length=100)
-    user_name = models.CharField(max_length=100)
+    first_name = models.CharField(max_length=200)
+    surname_name = models.CharField(max_length=200)
     sex = models.CharField(max_length=200)
     smoker = models.CharField(max_length=200)
     region = models.CharField(max_length=200)
     weight = models.CharField(max_length=200)
     height = models.CharField(max_length=200) 
     sex = models.CharField(max_length=200)
-    user_name = models.ForeignKey(Login_and_SignUp, null=True, on_delete=models.SET_NULL)
+    email = models.ForeignKey(SignUp, null=True, on_delete=models.SET_NULL)
+    user_name = models.ForeignKey(SignUp, null=True, on_delete=models.SET_NULL)
     
     def __str__(self):
         return self.user_name
 
-class SignUp(models.Model):
-    user_name = models.CharField(max_length=100)
-    password = models.CharField(max_length=100)
+
