2e7661de91bac6dbaa9b7ec70665ed3ae6b08f7a
MichAdebayo <adebayomichael4@gmail.com>
2025-01-23 09:42:35 +0100

    refined user progress logic

--- brief_app/insurance_app/models.py
+++ brief_app/insurance_app/models.py
@@ -2,14 +2,30 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class UserProfile(AbstractUser):
-    smoker = models.BooleanField(default=False)
-    region = models.CharField(max_length=10)
-    sex = models.BooleanField(default=False)
+    class SmokerType(models.TextChoices):
+        YES = 'Yes'
+        NO = 'No'
+
+    class RegionType(models.TextChoices):
+        NORTHEAST = 'Northeast'
+        NORTHWEST = 'Northwest'
+        SOUTHEAST = 'Southeast'
+        SOUTHWEST = 'Southwest'
+
+    class SexType(models.TextChoices):
+        MALE = 'Male'
+        FEMALE = 'Female'
+
     age = models.IntegerField(default=False)
     weight = models.IntegerField(default=False)
     height = models.IntegerField(default=False) 
+    smoker = models.CharField(blank=True, choices=SmokerType.choices, max_length=10)
+    region = models.CharField(blank=True, choices=RegionType.choices, max_length=10)
+    sex = models.CharField(blank=True, choices=SexType.choices, max_length=10)
 
     def __str__(self):
         return self.username
+
+ 
     
 
--- brief_app/insurance_app/views.py
+++ brief_app/insurance_app/views.py
@@ -1,8 +1,8 @@
-from django.shortcuts import render, redirect
-from django.contrib.auth import login
-from django.contrib.auth.views import LoginView # LogoutView
-from django.views.generic.edit import FormView
-#from django.contrib.auth.forms import BaseUserCreationForm
+# from django.shortcuts import render, redirect
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.views import LoginView #, LogoutView
+from django.views.generic.edit import CreateView, UpdateView
+from django.contrib import messages
 from django.urls import reverse_lazy
 from django.views.generic import TemplateView
 from .models import UserProfile
@@ -12,45 +12,53 @@ from .forms import UserProfileForm, UserSignupForm, UserLoginForm
 class HomeView(TemplateView):
     template_name = 'insurance_app/home.html'         # Template for the home page
 
-# class SignupView(FormView):
-#     template_name = 'insurance_app/signup.html'       # Template for the signup page
-#     form_class = CustomUserCreationForm       # Form to use for signup
-
-#     def form_valid(self, form):         # Save the user to the database
-#         user = form.save()              # Automatically log in the user after signup
-#         login(self.request, user)       # Redirect to the home page
-#         return redirect('login')
-
-class SignupView(FormView):
-    template_name = 'insurance_app/signup.html'
+class SignupView(CreateView):
+    model = UserProfile
     form_class = UserSignupForm  # Utilisez un formulaire personnalisé
-    redirect_authenticated_user = True  # Redirect already logged-in users
+    template_name = 'insurance_app/signup.html'
     success_url = reverse_lazy('login')
-
-    # def form_valid(self, form):
-    #     user = form.save()  # Enregistrez l'utilisateur
-    #     login(self.request, user)  # Connectez automatiquement l'utilisateur
-    #     return redirect('login')  # Redirigez vers la page de profil
-    
-class CustomLoginView(LoginView):
-    template_name = 'insurance_app/login.html'        # Template for the login page
-    form_class = UserLoginForm
     redirect_authenticated_user = True  # Redirect already logged-in users
+
+    def form_valid(self, form):         # Save the user to the database
+        self.object = form.save()
+        self.request.session['initial_user_profile'] = {
+            'username': self.object.username,
+            'email': self.object.email,
+            'password' : self.object.password,
+        }
+        return super().form_valid(form)
+
+class CustomLoginView(LoginView):
+    form_class = UserLoginForm
+    template_name = 'insurance_app/login.html'  # Template for the login page
     success_url = reverse_lazy('home')     # Replace 'home' with the name of your desired URL
+    redirect_authenticated_user = True  # Redirect already logged-in users
 
 # Create your views here.
-class UserProfileView(FormView):
+class UserProfileView(UpdateView): # LoginRequiredMixin, 
     model = UserProfile # Specify the model to use
-    template_name = 'insurance_app/user_profile.html' # Specify the template
-    context_object_name = 'users' # The name to use in the template
     form_class = UserProfileForm
+    template_name = 'insurance_app/user_profile.html' # Specify the template
+    success_url = reverse_lazy('user_profile') 
+    # redirect_authenticated_user = True
 
-# # Create your views here.
-# from django.shortcuts import render
+    def get_initial(self):
+        initial = super().get_initial()
+        if 'initial_user_profile' in self.request.session:
+            initial.update(self.request.session.pop('initial_user_profile'))
+            self.request.session.modified = True
+        return initial
+    
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        messages.success(self.request, 'Your profile has been updated!')
+        return response
+  
+    def get_object(self, queryset=None):
+        return self.request.user.userprofile
 
-# def welcome(request):
-#     return render(request, 'insurance_app/welcome.html')
-# from django.shortcuts import render
+
+
 
 
 
