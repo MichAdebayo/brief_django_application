ae879fd85bb5aa83114715287cd5d0f6959aaec0
MichAdebayo <adebayomichael4@gmail.com>
2025-01-27 12:08:51 +0100

    updating latest urls.py

--- brief_app/insurance_app/urls.py
+++ brief_app/insurance_app/urls.py
@@ -1,15 +1,14 @@
 from django.urls import path, include
-from .views import CustomLoginView, SignupView, HomeView, UserProfileView,WelcomeView,AboutView, JoinUsView,ApplyView,TemplateView,apply,contact_view,HealthAdvicesView,CybersecurityAwarenessView
+from .views import CustomLoginView, SignupView, HomeView, UserProfileView, AboutView, JoinUsView, ApplyView, TemplateView, apply,contact_view, HealthAdvicesView, CybersecurityAwarenessView, ChangePasswordView
 from django.contrib.auth.views import LogoutView
 from django.views.generic.base import RedirectView
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
     path('signup/', SignupView.as_view(), name='signup'),
-    # path('login/', CustomLoginView.as_view(), name='login'),
+    path('login/', CustomLoginView.as_view(), name='login'), # REPLACE WITH DOROTHEE'S CODE
     # path('logout/', LogoutView.as_view(), name='logout'),
-    path('user_profile/', UserProfileView.as_view(), name='user_profile'),
-    #path('welcome/',WelcomeView.as_view(), name='welcome'),
+    path('profile/', UserProfileView.as_view(), name='profile'),
     path('about/',AboutView.as_view(), name='about'),
     path('join-us/', JoinUsView.as_view(), name='join_us'),
     path('apply/', apply, name='apply'),
@@ -18,5 +17,5 @@ urlpatterns = [
     path('contact/', contact_view, name='contact'),
     path('health-advices/', HealthAdvicesView.as_view(), name='health_advices'),
     path('cybersecurity-awareness/',CybersecurityAwarenessView.as_view(), name='cybersecurity_awareness'),
-# path("__reload__/", include("django_browser_reload.urls")),
+    path('changepassword/', ChangePasswordView.as_view(), name='changepassword'),
 ]
